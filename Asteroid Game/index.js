6987c35b8cefa74e1b5d29168ba0594a1e1ab6f7
GurmanKD <gurmankaur0603@gmail.com>
2023-07-23 19:15:51 +0530

    added fuctionalities oh the object

--- Asteroid Game/index.js
+++ Asteroid Game/index.js
@@ -4,6 +4,175 @@ const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-c.fillStyle = "black";
-c.fillRect(0, 0, canvas.width, canvas.height);
 //dist from right, dist from top, width, height
+
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position; // {x, y}
+    this.velocity = velocity;
+    this.rotation = 0;
+  }
+
+  draw() {
+    c.save(); // if we dont use save and restore the whole canvas gets rotated
+    c.translate(this.position.x, this.position.y); //to move to the position of object to be rotated instead of default top left corner of the canvas
+    c.rotate(this.rotation);
+    c.translate(-this.position.x, -this.position.y); // translate back
+
+    c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+    c.fillStyle = "red";
+    c.fill();
+
+    c.beginPath();
+    c.moveTo(this.position.x + 30, this.position.y);
+    c.lineTo(this.position.x - 10, this.position.y - 10);
+    c.lineTo(this.position.x - 10, this.position.y + 10);
+    c.closePath();
+
+    c.strokeStyle = "white";
+    c.stroke();
+
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 5;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.closePath();
+    c.fillStyle = "white";
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+const player = new Player({
+  position: { x: canvas.width / 2, y: canvas.height / 2 },
+  velocity: { x: 0, y: 0 },
+});
+player.draw();
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+const SPEED = 3;
+const ROTATIONAL_SPEED = 0.05;
+const FRICTION = 0.97;
+const PROJECTILE_SPEED = 3;
+
+const projectiles = [];
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  //to get a new slate to write on everytime ie to avoid the smail trail
+
+  player.update();
+
+  for (let i = projectiles.length - 1; i >= 0; i--) {
+    const projectile = projectiles[i];
+    projectile.update();
+
+    // garbage collection of projectiles
+    if (
+      projectile.position.x + projectile.radius < 0 ||
+      projectile.position.x - projectile.radius > canvas.width ||
+      projectile.position.y - projectile.radius > canvas.height ||
+      projectile.position.y + projectile.radius < 0
+    ) {
+      projectiles.splice(i, 1);
+    }
+  }
+  // using for instead od forEach to render things from the back of the array and avoid flash due to jumping og the elements ////DOUBT////
+
+  // player.velocity.x = 0;
+  // player.velocity.y = 0;
+
+  if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED;
+    player.velocity.y = Math.sin(player.rotation) * SPEED;
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= FRICTION; // to not stot immediately as we lift up our finger
+    player.velocity.y *= FRICTION;
+  }
+
+  if (keys.d.pressed) {
+    player.rotation += ROTATIONAL_SPEED;
+  } else if (keys.a.pressed) {
+    player.rotation -= ROTATIONAL_SPEED;
+  }
+}
+
+animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      keys.w.pressed = true;
+      break;
+    case "KeyA":
+      keys.a.pressed = true;
+      break;
+    case "KeyD":
+      keys.d.pressed = true;
+      break;
+    case "Space":
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + Math.cos(player.rotation) * 30,
+            y: player.position.y + Math.sin(player.rotation) * 30,
+          },
+          velocity: {
+            x: Math.cos(player.rotation) * PROJECTILE_SPEED,
+            y: Math.sin(player.rotation) * PROJECTILE_SPEED,
+          },
+        })
+      );
+      break;
+  }
+  console.log(event);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      keys.w.pressed = false;
+      break;
+    case "KeyA":
+      keys.a.pressed = false;
+      break;
+    case "KeyD":
+      keys.d.pressed = false;
+      break;
+  }
+  console.log(event);
+});
